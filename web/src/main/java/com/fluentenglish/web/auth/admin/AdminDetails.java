4388d47c7d6d039a525791a7eec0a32506017e2a
Trần Sĩ  Tiến <106511449+TranSiTien@users.noreply.github.com>
2023-09-22 17:23:56 +0700

    Update AdminDetails.java

--- web/src/main/java/com/fluentenglish/web/auth/admin/AdminDetails.java
+++ web/src/main/java/com/fluentenglish/web/auth/admin/AdminDetails.java
@@ -14,30 +14,37 @@ public class AdminDetails implements UserDetails {
     private String password;
     private boolean enabled;
     private Collection<? extends GrantedAuthority> authorities;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return authorities;
     }
+
     @Override
     public String getPassword() {
         return password;
     }
+
     @Override
     public String getUsername() {
         return email;
     }
+
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
+
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
+
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
+
     @Override
     public boolean isEnabled() {
         return enabled;
