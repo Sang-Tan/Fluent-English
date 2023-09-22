e578cfcd0fadcaa39863bbee98f84b356814d559
Trần Sĩ  Tiến <106511449+TranSiTien@users.noreply.github.com>
2023-09-22 17:27:50 +0700

    Update AdminDetails.java

--- web/src/main/java/com/fluentenglish/web/auth/admin/AdminDetails.java
+++ web/src/main/java/com/fluentenglish/web/auth/admin/AdminDetails.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-@EqualsAndHashCode
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Builder
 public class AdminDetails implements UserDetails {
     private Integer id;
@@ -46,6 +46,7 @@ public class AdminDetails implements UserDetails {
     }
 
     @Override
+    @EqualsAndHashCode.Include
     public boolean isEnabled() {
         return enabled;
     }
