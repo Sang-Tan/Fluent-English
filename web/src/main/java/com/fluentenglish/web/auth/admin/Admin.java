bca924f33465e195d4624f96bf93babcec6d7c6b
Trần Sĩ  Tiến <106511449+TranSiTien@users.noreply.github.com>
2023-09-22 17:27:53 +0700

    Update Admin.java

--- web/src/main/java/com/fluentenglish/web/auth/admin/Admin.java
+++ web/src/main/java/com/fluentenglish/web/auth/admin/Admin.java
@@ -1,10 +1,7 @@
 package com.fluentenglish.web.auth.admin;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "admins",
@@ -13,8 +10,9 @@ import lombok.Setter;
         })
 @Getter
 @Setter
-@AllArgsConstructor
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class Admin {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
