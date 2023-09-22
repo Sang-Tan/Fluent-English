9d0f33b327a1b54a26d0b7c64809b33080850b99
Trần Sĩ  Tiến <106511449+TranSiTien@users.noreply.github.com>
2023-09-22 17:12:12 +0700

    Update V22_1__create_admins_table.sql

--- web/src/main/resources/db/migration/V22_1__create_admins_table.sql
+++ web/src/main/resources/db/migration/V22_1__create_admins_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS admins
 (
     id SERIAL PRIMARY KEY,
-    created_date TIMESTAMP(6) DEFAULT now(),
+    created_date TIMESTAMP(6) DEFAULT now() NOT NULL,
     email        VARCHAR(255) UNIQUE NOT NULL,
     enabled      BOOLEAN      NOT NULL DEFAULT TRUE,
     full_name    VARCHAR(50) NOT NULL,
