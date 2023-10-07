8a76bd581ccae67a2bd82060c52cd05a387f92f1
Trần Sĩ  Tiến <106511449+TranSiTien@users.noreply.github.com>
2023-10-07 20:32:28 +0700

    hotfix: table didn't displayed

--- web/src/main/java/com/fluentenglish/web/common/datatable/DataTableRow.java
+++ web/src/main/java/com/fluentenglish/web/common/datatable/DataTableRow.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Random;
+
 @Builder
 @Getter
 @Setter
@@ -12,6 +14,7 @@ import lombok.Setter;
 public class DataTableRow {
     private Integer id;
     private String name;
+    private Integer position;
     private String detailUrl;
     private String updateUrl;
     private String deleteUrl;
--- web/src/main/resources/static/js/fragments/data_table.js
+++ web/src/main/resources/static/js/fragments/data_table.js
@@ -1,12 +1,69 @@
+
+function initializeDataTable(id, config) {
+    let dataTableConfig;
+    if(config.isArrangeable) {
+        dataTableConfig = {
+            paging: config.isPaginable,
+            rowReorder: {
+                enabled: true,
+                selector: 'tr'
+            },
+            fixedColumns: true,
+            columnDefs: [{ width: 700, targets: -3 }]
+        }
+        // build a success message notification
+        const successDiv = $('<div>', {
+            id: 'success-reposition',
+            class: 'alert alert-success',
+            text: 'Successfully saved',
+            style: 'display: none'
+        });
+        // build a fail message notification
+        const failDiv = $('<div>', {
+            id: 'fail-reposition',
+            class: 'alert alert-danger',
+            text: 'Failed to save',
+            style: 'display: none'
+        });
+        const btn = createSavePositionButton(id, config.changePositionsUrl, successDiv, failDiv);
+        btn.after(successDiv);
+        btn.after(failDiv);
+    }
+    else {
+        dataTableConfig = {
+            paging: config.isPaginable,
+            fixedColumns: true,
+            columnDefs: [{ width: 700, targets: -3 }]
+        }
+    }
+    new DataTable('#' + id, dataTableConfig);
+}
+
+function createSavePositionButton(tableId, changePositionsUrl, successDiv, failDiv) {
+    const btn = $('<button>', {
+        class: 'btn btn-primary',
+        text: 'Save Position',
+        click: function () {
+            originalDatatablesMap[tableId].savePosition(successDiv, failDiv);
+        }
+    })
+
+    // Add the button after the table
+    $('#' + tableId).after(btn);
+    return btn;
+}
+
+
+
 class DataTableManager {
-    constructor(changePositionUrl) {
-        this.originalPositionMap = this.getCurrentPositionMap();
-        this.changedPositionMap = {};
+    constructor(id, changePositionUrl) {
+        this.id = id;
         this.changePositionUrl = changePositionUrl;
+        this.originalPositionMap = this.getCurrentPositionMap();
     }
 
     getCurrentPositionMap() {
-        const currentData = $('#' + DATA_TABLE_ID).DataTable().rows().data();
+        const currentData = $('#' + this.id).DataTable().rows().data();
         const orderMap = {};
         for (let i = 0; i < currentData.length; i++) {
             orderMap[currentData[i][1]] = currentData[i][0];
@@ -14,73 +71,53 @@ class DataTableManager {
         return orderMap;
     }
 
-    trackChange(id, newPosition) {
-        this.changedPositionMap[id] = newPosition;
-    }
 
-// Function to send only the changed data to the server
-    sendChangesToServer() {
-// Wrap changedDataMap in an object with the key "idPositionMap"
-        const jsonData = JSON.stringify({"idPositionMap": this.changedPositionMap});
+    // Function to send only the changed data to the server
+    sendChangesToServer(successDiv, failDiv, changedPositionMap) {
+        // Wrap changedDataMap in an object with the key "idPositionMap"
+        const jsonData = JSON.stringify({"idPositionMap": changedPositionMap});
 
-// Reference to the success and fail message divs
-        const successDiv = document.getElementById("success-reposition");
-        const failDiv = document.getElementById("fail-reposition");
 
-// Send the JSON data to the server using AJAX
+        // Send the JSON data to the server using AJAX
         $.ajax({
             type: "POST",
             url: this.changePositionUrl,
             contentType: "application/json",
             data: jsonData,
-            success: function (response) {
-// Show the success message div and hide the fail message div
-                successDiv.style.display = "block";
-                failDiv.style.display = "none";
+            success: function () {
+                // Show the success message div and hide the fail message div
+                successDiv.css("display", "block");
+                failDiv.css("display", "none");
             },
-            error: function (error) {
-// Show the fail message div and hide the success message div
-                failDiv.style.display = "block";
-                successDiv.style.display = "none";
+            error: function () {
+                // Show the fail message div and hide the success message div
+                successDiv.css("display", "none");
+                failDiv.css("display", "block");
             }
         });
     }
-// Function to save a position change and track it
-    savePosition() {
+    // Function to save a position change and track it
+    savePosition(successDiv, failDiv) {
         const currentPositionMap = this.getCurrentPositionMap();
 
-// Check if the position has changed
+        let changedPositionMap = {};
+        // Check if the position has changed
         for (const id in currentPositionMap) {
             if (currentPositionMap[id] !== this.originalPositionMap[id]) {
-                this.trackChange(id, currentPositionMap[id]);
+                changedPositionMap[id] = currentPositionMap[id];
             }
         }
 
-// check if there are any changes to send
-        if (Object.keys(this.changedPositionMap).length > 0) {
-            this.sendChangesToServer();
+        // check if there are any changes to send
+        if (Object.keys(changedPositionMap).length > 0) {
+            this.sendChangesToServer( successDiv, failDiv, changedPositionMap);
         }
     }
 }
 
-const DATA_TABLE_ID = 'data_table';
-const IS_PAGINABLE = [[${dataTable.isPaginable}]];
-const IS_ARRANGEABLE = [[${dataTable.isArrangeable}]];
-const CHANGE_POSITION_URL = "[[${dataTable.changePositionsUrl}]]";
-function initializeDataTable() {
-    const dataTableConfig = {
-        paging: IS_PAGINABLE,
-        rowReorder: {
-            selector: 'tr',
-            enabled: IS_ARRANGEABLE
-        },
-        fixedColumns: true,
-        columnDefs: [{ width: 700, targets: -3 }]
-    };
-
-    new DataTable('#' + DATA_TABLE_ID, dataTableConfig);
-}
-
-const dataTableManager = new DataTableManager(CHANGE_POSITION_URL);
+let originalDatatablesMap = {};
 // Call the initializeDataTable function to initialize the DataTable
-initializeDataTable();
+for (const each of dataTables) {
+    initializeDataTable(each.id, each.config);
+    originalDatatablesMap[each.id] = new DataTableManager(each.id, each.config.changePositionsUrl);
+}
