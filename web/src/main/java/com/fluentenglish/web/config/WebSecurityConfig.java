d870ce5d1c36cc7bdbb7cfb7042858705c4e1ab1
SPRV <sangprovai14@gmail.com>
2023-12-27 17:14:01 +0700

    feat(be): jwt security configuration add jwt authentication support in web remove username and password login configuration

--- web/src/main/java/com/fluentenglish/web/config/WebSecurityConfig.java
+++ web/src/main/java/com/fluentenglish/web/config/WebSecurityConfig.java
@@ -2,23 +2,24 @@ package com.fluentenglish.web.config;
 
 import com.fluentenglish.web.auth.admin.AdminDetailsService;
 import com.fluentenglish.web.auth.Role;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.fluentenglish.web.auth.filter.BearerTokenAuthenticationFilter;
+import com.fluentenglish.web.auth.token.jwt.JWTAuthenticationProvider;
+import com.fluentenglish.web.auth.token.jwt.JWTProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
+import org.springframework.security.web.context.SecurityContextHolderFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -29,46 +30,36 @@ public class WebSecurityConfig {
                   "/css/**", "/js/**", "/images/**", "/webjars/**");
     }
     @Bean
-    public SecurityFilterChain adminSecurityFilterChain(HttpSecurity http, @Autowired AdminDetailsService adminDetailsService
-            , @Autowired RememberMeConfiguration rememberMeConfiguration ) throws Exception {
+    public SecurityFilterChain adminSecurityFilterChain(HttpSecurity http,
+                                                        JWTProcessor jwtProcessor) throws Exception {
+        AuthenticationManager authenticationManager = authManager(http, jwtProcessor);
+
         http.csrf(AbstractHttpConfigurer::disable);
+        http.authenticationManager(authenticationManager)
+                        .sessionManagement((sessionManagement) -> sessionManagement
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        .addFilterAfter(new BearerTokenAuthenticationFilter(authenticationManager), SecurityContextHolderFilter.class);
+
         http.securityMatcher("/admin/**")
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/admin/**").hasRole(Role.ADMIN.name())
-                )
-                .formLogin(formLogin -> formLogin
-                        .loginProcessingUrl("/admin/login")
-                        .loginPage("/admin/login")
-                        .defaultSuccessUrl("/admin")
-                        .failureUrl("/admin/login?badCredentials=true")
-                        .permitAll()
-                )
-                .rememberMe((rememberMe) -> rememberMe
-                        .tokenValiditySeconds(rememberMeConfiguration.getRememberMeTokenValiditySeconds())
-                        .key(rememberMeConfiguration.getRememberMeKey())
-                        .rememberMeServices(new TokenBasedRememberMeServices(rememberMeConfiguration.getRememberMeKey(), adminDetailsService))
-                )
-                .authenticationManager(getUserAuthManager(adminDetailsService, http))
-                .sessionManagement((sessionManagement) -> sessionManagement.invalidSessionUrl("/admin/login"))
-                .logout((logout) -> logout.logoutUrl("/admin/logout")
-                        .logoutSuccessUrl("/admin/login?logoutSuccess=true")
-                        .deleteCookies("JSESSIONID")
-                        .permitAll());
+                        .requestMatchers("/admin/api/login").permitAll()
+                        .anyRequest().hasRole(Role.ADMIN.name())
+                );
+
         return http.build();
-    }
-    private  DaoAuthenticationProvider getAuthProvider(UserDetailsService userDetailsService) {
-        final DaoAuthenticationProvider customAuthProvider = new DaoAuthenticationProvider();
-        customAuthProvider.setUserDetailsService(userDetailsService);
-        customAuthProvider.setPasswordEncoder(getPasswordEncoder());
-        return customAuthProvider;
-    }
-    private AuthenticationManager getUserAuthManager(UserDetailsService userDetailsService, HttpSecurity http) throws Exception{
-        return http.getSharedObject(AuthenticationManagerBuilder.class)
-                .authenticationProvider(getAuthProvider(userDetailsService))
-                .build();
     }
     @Bean
     public PasswordEncoder getPasswordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    private AuthenticationManager authManager(HttpSecurity http, JWTProcessor jwtProcessor) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .authenticationProvider(jwtAuthProvider(jwtProcessor))
+                .build();
+    }
+
+    private AuthenticationProvider jwtAuthProvider(JWTProcessor jwtProcessor) {
+        return new JWTAuthenticationProvider(jwtProcessor);
     }
 }
