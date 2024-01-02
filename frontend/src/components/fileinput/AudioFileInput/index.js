93a91772b9df75b3a45d24ee18fa393bc38c2e8f
SPRV <sangprovai14@gmail.com>
2024-01-02 08:45:53 +0700

    feat(fe-admin): audio file input component

---
+++ frontend/src/components/fileinput/AudioFileInput/index.js
@@ -0,0 +1,67 @@
+import { useState, useRef } from "react";
+import { toast } from "react-toastify";
+import { PencilSquare, ArrowClockwise } from "react-bootstrap-icons";
+import AudioPlayer from "src/components/AudioPlayer";
+import classNames from "classnames/bind";
+import styles from "./AudioFileInput.module.scss";
+
+const cx = classNames.bind(styles);
+
+function AudioFileInput({
+  initialAudioUrl = null,
+  onUploadUrlChange = (url) => {},
+}) {
+  const [audioUrl, setAudioUrl] = useState(initialAudioUrl);
+  const fileInputRef = useRef(null);
+
+  const changeAudioUrl = (url) => {
+    if (audioUrl && audioUrl.startsWith("blob:")) {
+      URL.revokeObjectURL(audioUrl);
+    }
+    setAudioUrl(url === null ? initialAudioUrl : url);
+    onUploadUrlChange(url);
+  };
+
+  const handleFileChange = (event) => {
+    if (event.target.files.length === 0) {
+      return;
+    }
+    const file = event.target.files[0];
+    if (file && file.type.startsWith("audio/")) {
+      changeAudioUrl(URL.createObjectURL(file));
+    } else {
+      toast.error("Please select an audio file.");
+    }
+  };
+
+  const handleReset = () => {
+    changeAudioUrl(null);
+    fileInputRef.current.value = null;
+  };
+
+  return (
+    <div className={cx("container")}>
+      <div className={cx("options")}>
+        <input
+          type="file"
+          accept="audio/*"
+          onChange={handleFileChange}
+          ref={fileInputRef}
+          hidden
+        />
+        <button
+          className={cx("button", "button-upload")}
+          onClick={() => fileInputRef.current.click()}
+        >
+          <PencilSquare />
+        </button>
+        <button className={cx("button", "button-reset")}>
+          <ArrowClockwise onClick={handleReset} />
+        </button>
+      </div>
+      <AudioPlayer audioUrl={audioUrl} />
+    </div>
+  );
+}
+
+export default AudioFileInput;
