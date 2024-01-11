ded2e6d5d676d12f5c70a393ced73bf15a583811
SPRV <sangprovai14@gmail.com>
2024-01-11 08:06:12 +0700

    feat(be): logging uncaught exception

--- web/src/main/java/com/fluentenglish/web/common/exception/errorresponse/ResponseExceptionHandler.java
+++ web/src/main/java/com/fluentenglish/web/common/exception/errorresponse/ResponseExceptionHandler.java
@@ -1,17 +1,14 @@
 package com.fluentenglish.web.common.exception.errorresponse;
 
 import com.fluentenglish.web.common.exception.userinput.UserInputException;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 
-import java.util.Date;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
+@Log4j2
 @RestControllerAdvice
 public class ResponseExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
@@ -40,8 +37,8 @@ public class ResponseExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ResponseEntity<Object> handleAllUncaughtException(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorResponse.builder()
-                .build());
+    public ResponseEntity<Void> handleAllUncaughtException(Exception ex) {
+        log.error("Uncaught exception [ERROR: {}]", ex.getMessage(), ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
 }
