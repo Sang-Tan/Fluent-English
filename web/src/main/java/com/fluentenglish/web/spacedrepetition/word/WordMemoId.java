b5f948b74a062e4deb286af9534f20a7535cc044
SPRV <sangprovai14@gmail.com>
2024-02-05 13:15:04 +0700

    feat(be): word memo entity and repo

---
+++ web/src/main/java/com/fluentenglish/web/spacedrepetition/word/WordMemoId.java
@@ -0,0 +1,41 @@
+package com.fluentenglish.web.spacedrepetition.word;
+
+import com.fluentenglish.web.learning.material.word.Word;
+import com.fluentenglish.web.user.User;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Embeddable
+public class WordMemoId implements Serializable {
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "word_id", referencedColumnName = "id")
+    private Word word;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WordMemoId that = (WordMemoId) o;
+        return Objects.equals(user.getId(), that.user.getId()) &&
+                Objects.equals(word.getId(), that.word.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user.getId(), word.getId());
+    }
+}
