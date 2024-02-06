7cc0e024ebfa978dff98b0cc4b2b919da04035ba
SPRV <sangprovai14@gmail.com>
2024-02-06 15:48:57 +0700

    feat(be): create convenient entities' constructor

--- web/src/main/java/com/fluentenglish/web/spacedrepetition/word/WordMemoId.java
+++ web/src/main/java/com/fluentenglish/web/spacedrepetition/word/WordMemoId.java
@@ -6,7 +6,9 @@ import jakarta.persistence.Embeddable;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
@@ -14,6 +16,8 @@ import java.util.Objects;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Embeddable
 public class WordMemoId implements Serializable {
 
