88acf219258a971d2c07ed9bd9d4d04dcbfb8728
Trần Sĩ  Tiến <106511449+TranSiTien@users.noreply.github.com>
2024-02-06 11:40:39 +0700

    feat(BE): user info service

---
+++ web/src/main/java/com/fluentenglish/web/gaming/user/UserInfoService.java
@@ -0,0 +1,65 @@
+package com.fluentenglish.web.gaming.user;
+
+import com.fluentenglish.web.common.exception.errorresponse.NotFoundException;
+import com.fluentenglish.web.gaming.user.dto.CurrentStateDto;
+import com.fluentenglish.web.gaming.user.dto.LevelBeforeAfterDto;
+import com.fluentenglish.web.gaming.user.dto.LevelProgressDto;
+import com.fluentenglish.web.gaming.user.dto.UserAttributesDto;
+import com.fluentenglish.web.gaming.user.level.UserLevel;
+import com.fluentenglish.web.gaming.user.level.UserLevelRepository;
+import com.fluentenglish.web.user.User;
+import com.fluentenglish.web.user.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserInfoService {
+    private final UserRepository userRepository;
+    private final UserLevelRepository userLevelRepository;
+
+    public UserInfoService(UserRepository userRepository,
+                           UserLevelRepository userLevelRepository) {
+        this.userRepository = userRepository;
+        this.userLevelRepository = userLevelRepository;
+    }
+
+    public UserAttributesDto getUserAttributes(Integer userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new NotFoundException("User not found"));
+        return new UserAttributesDto(user.getLevel().getBaseHp(), user.getLevel().getBaseShield());
+    }
+
+    public CurrentStateDto getCurrentState(Integer userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new NotFoundException("User not found"));
+        return new CurrentStateDto(user.getCurrentHpPercent());
+    }
+
+    public LevelProgressDto getLevelProgress(Integer userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new NotFoundException("User not found"));
+        return new LevelProgressDto(user.getLevel().getLevel(), user.getExperience());
+    }
+
+    public LevelBeforeAfterDto addExperience(Integer userId, Integer experienceGained) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new NotFoundException("User not found"));
+        LevelProgressDto before = new LevelProgressDto(user.getLevel().getLevel(), user.getExperience());
+
+        user.setExperience(user.getExperience() + experienceGained);
+        UserLevel nextLevel = userLevelRepository.findById(user.getLevel().getLevel() + 1).orElse(null);
+
+        while(user.getExperience() >= user.getLevel().getExpToNextLevel() && nextLevel != null){
+            user.setExperience(user.getExperience() - user.getLevel().getExpToNextLevel());
+            user.setLevel(nextLevel);
+            nextLevel = userLevelRepository.findById(user.getLevel().getLevel() + 1).orElse(null);
+        }
+        LevelProgressDto after = new LevelProgressDto(user.getLevel().getLevel(), user.getExperience());
+
+        userRepository.save(user);
+        return new LevelBeforeAfterDto(before, after);
+    }
+
+    public void updateCurrentHp(Integer userId, Integer currentHp) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new NotFoundException("User not found"));
+        if(currentHp < 0 || currentHp > user.getLevel().getBaseHp()) {
+            user.setCurrentHpPercent(currentHp/ (float) user.getLevel().getBaseHp());
+            userRepository.save(user);
+        }
+    }
+}
--- web/src/main/java/com/fluentenglish/web/gaming/user/level/UserLevelRepository.java
+++ web/src/main/java/com/fluentenglish/web/gaming/user/level/UserLevelRepository.java
@@ -2,5 +2,8 @@ package com.fluentenglish.web.gaming.user.level;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserLevelRepository extends JpaRepository<UserLevel, Integer> {
+    Optional<UserLevel> findByLevel(Integer level);
 }
