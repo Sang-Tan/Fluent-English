58eb485070d471b060147cb260f6e4415c4eb196
SPRV <sangprovai14@gmail.com>
2024-02-12 08:08:16 +0700

    feat(be): update session words scores

--- web/src/main/java/com/fluentenglish/web/study/session/score/RedisSessionWordsScores.java
+++ web/src/main/java/com/fluentenglish/web/study/session/score/RedisSessionWordsScores.java
@@ -6,6 +6,9 @@ import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @Component
 @Scope("prototype")
 public class RedisSessionWordsScores extends RedisStudySessionObject implements SessionWordsScores {
@@ -26,18 +29,37 @@ public class RedisSessionWordsScores extends RedisStudySessionObject implements 
             throw new RuntimeException("Word score's key does not exist or the it " +
                     "has been set in the same transaction");
         }
-        String[] wordScoreArray = wordScore.split("\\|");
 
-        WordScore score = new WordScore();
-        score.setTotalScore(Integer.parseInt(wordScoreArray[0]));
-        score.setAttempts(Integer.parseInt(wordScoreArray[1]));
+        return stringToWordScore(wordScore);
+    }
 
-        return score;
+    @Override
+    public Map<Integer, WordScore> getWordsScores() {
+        Map<Integer, String> scoreMap = hashOperations.entries(getWordScoreKey());
+        return scoreMap.entrySet().stream()
+                .collect(
+                        Collectors.toMap(
+                                Map.Entry::getKey,
+                                entry -> stringToWordScore(entry.getValue())
+                        )
+                );
     }
 
     @Override
     public void setWordScore(int wordId, WordScore wordScore) {
-        hashOperations.put(getWordScoreKey(), wordId, wordScore.getTotalScore() + "|" + wordScore.getAttempts());
+        hashOperations.put(getWordScoreKey(), wordId, wordScoreToString(wordScore));
+    }
+
+    @Override
+    public void setWordsScores(Map<Integer, WordScore> wordScores) {
+        Map<Integer, String> stringWordScores = wordScores.entrySet().stream()
+                .collect(
+                        Collectors.toMap(
+                                Map.Entry::getKey,
+                                entry -> wordScoreToString(entry.getValue())
+                        )
+                );
+        hashOperations.putAll(getWordScoreKey(), stringWordScores);
     }
 
     @Override
@@ -52,5 +74,17 @@ public class RedisSessionWordsScores extends RedisStudySessionObject implements 
 
     private String getWordScoreKey() {
         return String.format("studySession:%s:wordsScores", getSessionMetadata().sessionId());
+    }
+
+    private WordScore stringToWordScore(String wordScore) {
+        String[] wordScoreArray = wordScore.split("\\|");
+        WordScore score = new WordScore();
+        score.setTotalScore(Integer.parseInt(wordScoreArray[0]));
+        score.setAttempts(Integer.parseInt(wordScoreArray[1]));
+        return score;
+    }
+
+    private String wordScoreToString(WordScore wordScore) {
+        return wordScore.getTotalScore() + "|" + wordScore.getAttempts();
     }
 }
