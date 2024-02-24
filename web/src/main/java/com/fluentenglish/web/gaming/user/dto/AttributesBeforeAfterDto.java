c1160422ecddaf0f4d4c6d0661ba233730179d8d
SPRV <sangprovai14@gmail.com>
2024-02-24 07:50:50 +0700

    feat(be): add user attributes to battle result

---
+++ web/src/main/java/com/fluentenglish/web/gaming/user/dto/AttributesBeforeAfterDto.java
@@ -0,0 +1,10 @@
+package com.fluentenglish.web.gaming.user.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+public class AttributesBeforeAfterDto {
+    private UserAttributesDto before;
+    private UserAttributesDto after;
+}
