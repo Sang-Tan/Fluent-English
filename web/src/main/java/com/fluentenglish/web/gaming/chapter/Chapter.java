2fb30f64f60abcfebda5cf01a64b8a0cfc1340d3
SPRV <sangprovai14@gmail.com>
2024-03-03 09:41:55 +0700

    hotfix(be): user entity

--- web/src/main/java/com/fluentenglish/web/gaming/chapter/Chapter.java
+++ web/src/main/java/com/fluentenglish/web/gaming/chapter/Chapter.java
@@ -3,12 +3,16 @@ package com.fluentenglish.web.gaming.chapter;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "chapters")
 public class Chapter {
     @Id
@@ -21,4 +25,8 @@ public class Chapter {
     private String storyEndEng;
 
     private String storyEndVi;
+
+    public Chapter(int number) {
+        this.number = number;
+    }
 }
--- web/src/main/java/com/fluentenglish/web/gaming/user/level/UserLevel.java
+++ web/src/main/java/com/fluentenglish/web/gaming/user/level/UserLevel.java
@@ -3,12 +3,16 @@ package com.fluentenglish.web.gaming.user.level;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "user_levels")
 public class UserLevel {
     @Id
@@ -19,4 +23,8 @@ public class UserLevel {
     private Integer baseHp;
 
     private Integer baseShield;
+
+    public UserLevel(int level) {
+        this.level = level;
+    }
 }
