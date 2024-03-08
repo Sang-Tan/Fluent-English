f0bef8f08d214be22e93621e8e40c609e3d3fe91
tien.nhat <tiennhat306@gmail.com>
2024-03-08 11:28:16 +0700

    hotfix: fix interface implement

--- web/src/main/java/com/fluentenglish/web/study/session/service/dto/StudySessionStatusDto.java
+++ web/src/main/java/com/fluentenglish/web/study/session/service/dto/StudySessionStatusDto.java
@@ -1,11 +1,14 @@
 package com.fluentenglish.web.study.session.service.dto;
 
+import com.fluentenglish.web.study.session.dao.battle.BattleUpdateInfo;
 import com.fluentenglish.web.study.session.dao.quiz.Quiz;
 
 
 public interface StudySessionStatusDto {
     Quiz getNextQuiz();
     void setNextQuiz(Quiz nextQuiz);
+    BattleUpdateInfo getBattleUpdateInfo();
+    void setBattleUpdateInfo(BattleUpdateInfo battleUpdateInfo);
     int getRemainingQuizzesCount();
     void setRemainingQuizzesCount(int remainingQuizzesCount);
 }
